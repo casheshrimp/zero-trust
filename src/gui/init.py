097d9b19e0e05523c131b83b70a982f0f9982418
casheshrimp <c9801724@gmail.com>
2026-01-16 01:09:02 +0300

    Update init.py

--- src/gui/init.py
+++ src/gui/init.py
@@ -1,23 +1,5 @@
 """
-GUI модуль
+Модуль графического интерфейса
 """
-
-try:
-    from .main_window import MainWindow
-except ImportError:
-    from PyQt6.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QWidget
-    from PyQt6.QtCore import Qt
-    
-    class MainWindow(QMainWindow):
-        def __init__(self):
-            super().__init__()
-            self.setWindowTitle("ZeroTrust Inspector")
-            self.setGeometry(100, 100, 800, 600)
-            
-            central = QWidget()
-            self.setCentralWidget(central)
-            layout = QVBoxLayout(central)
-            
-            label = QLabel("ZeroTrust Inspector GUI")
-            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-            layout.addWidget(label)
+from .main_window import MainWindow
+from .components.device_list import DeviceListWidget
